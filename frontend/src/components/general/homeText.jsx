2f80119fa0d3986c90b991b26adb35566ea58096
Felipe Quiroga <felipe.quiroga@torontomu.ca>
2024-02-24 16:19:17 -0500

    Created text box for home page

---
+++ frontend/src/components/general/homeText.jsx
@@ -0,0 +1,8 @@
+export default function HomeText() {
+    return (
+        <div className="rounded border border-gray-700 text-center p-5 m-5 w-1/3">
+            GymBro is designed for those individuals eager to kickstart their fitness journey and embrace a healthier lifestyle but find themselves unsure of where or how to begin. While numerous apps exist to track workouts and calories, none of them offer a personalized workout and meal plan feature to help people get started. That's where GymBro comes in!
+        </div>
+        
+    )
+}
