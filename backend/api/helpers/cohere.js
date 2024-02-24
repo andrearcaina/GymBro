e4c8c43a2f56924e6019d88b65294a4d17213009
Joseph Leung <leung.joseph@outlook.com>
2024-02-24 14:30:39 -0500

    added fetch helper functions for cohere

--- backend/api/helpers/cohere.js
+++ backend/api/helpers/cohere.js
@@ -1,4 +1,7 @@
-export const fetchChatRequest = async() => {
+// function fetching workout routine plan
+export const fetchWorkoutRoutine = async(goal) => {
+    const stringGoal = goal.toString();
+
     const response = await fetch('https://api.cohere.ai/v1/chat', {
         method: 'POST',
         headers: {
@@ -8,7 +11,7 @@ export const fetchChatRequest = async() => {
         },
         body: JSON.stringify({
             model: 'command',
-            message: `give me a workout routine for 5 days a week`,
+            message: `give me a workout routine for 5 days a week tailored towards ${stringGoal} with no rest days`,
         }),
     });
 
@@ -17,6 +20,33 @@ export const fetchChatRequest = async() => {
     }
 
     const data = await response.json();
+    console.log(data);
+    return data;
+};
 
+// function api fetching for meal plan
+export const fetchMealPlan = async(goal, dietRestrictions) => {
+    const stringRestrictions = dietRestrictions.toString();
+    const stringGoal = goal.toString();
+
+    const response = await fetch('https://api.cohere.ai/v1/chat', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+            Authorization: `Bearer ${process.env.COHERE_API_TOKEN}`,
+        },
+        body: JSON.stringify({
+            model: 'command',
+            message: `give me a 5 day diet/meal plan with ${stringRestrictions} dietary restrictions tailored towards ${stringGoal}. Include the calories per meal.`,
+        }),
+    });
+
+    if (!response.ok) {
+        throw new Error(`Failed to fetch: ${response.status} - ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    console.log(data);
     return data;
 };
--- backend/api/server.js
+++ backend/api/server.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from "cors";
-import { fetchChatRequest } from "./helpers/cohere.js"
+import { fetchWorkoutRoutine } from "./helpers/cohere.js"
+import { fetchMealPlan } from "./helpers/cohere.js"
 dotenv.config();
 
 const app = express();
@@ -13,18 +14,41 @@ app.get('/api', (req, res) => {
     res.send({"Hello": "World"});
 });
 
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
-});
+app.get("/api/workout/:goal", (req, res) => {
+    const goal = req.params.goal.toString();
 
-app.get(
-    "/chatreq",
-    async (req, res) => {  
-      fetchChatRequest()
+    if (!goal) {
+        res.status(400).json({ error: "Invalid goal" })
+        return
+      }
+
+    fetchWorkoutRoutine(goal)
         .then((response) => {
           res.json(response)
         })
         .catch((error) => {
           res.status(500).json({ error: error.name })
         })
+});
+
+app.get("/api/mealPlan/:goal/:dietRestrictions", (req, res) => {
+    const goal = req.params.goal.toString();
+    const dietRestrictions = req.params.dietRestrictions.toString();
+
+    if (!goal || !dietRestrictions) {
+        res.status(400).json({ error: "Invalid goal or dietary restriction" })
+        return
+      }
+
+      fetchMealPlan(goal, dietRestrictions)
+        .then((response) => {
+          res.json(response)
+        })
+        .catch((error) => {
+          res.status(500).json({ error: error.name })
+        })
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
 });
