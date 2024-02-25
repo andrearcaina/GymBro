51c9b9cc4f2590133980a89c03eefd0f2a98fde9
Joseph Leung <leung.joseph@outlook.com>
2024-02-24 20:17:21 -0500

    added api call from backend

--- frontend/src/app/roadmap/page.js
+++ frontend/src/app/roadmap/page.js
@@ -1,5 +1,21 @@
+'use client';
+import { RoadMapForm } from "@/components"
+import { useState } from 'react';
+
 export default function Roadmap(){
+    const [data, setData] = useState({});
+
+    const handleFormSubmit = (e) => {
+        setData(e);
+    }
+
     return(
-        <main>HEllo loquito</main>
+        <main>
+            <RoadMapForm onSubmit={handleFormSubmit}/>
+            {Object.keys(data).length !== 0 ? (
+            <p>{data["Day 1"]["Dinner"]}</p>
+            ) : 
+            <p>failed</p>}
+        </main>
     )
 }
