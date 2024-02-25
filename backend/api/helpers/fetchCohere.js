1caf28ce1c6c4cb542403cb6d80ff11f5a9bad29
Joseph Leung <leung.joseph@outlook.com>
2024-02-24 22:45:16 -0500

    Update fetchCohere.js

--- backend/api/helpers/fetchCohere.js
+++ backend/api/helpers/fetchCohere.js
@@ -4,7 +4,7 @@ import { generateMeals, generateWorkout } from '../utils/parse.js';
 export const fetchWorkoutRoutine = async (goal) => {
     const stringGoal = goal.toString();
 
-    const response = await fetch('https://api.cohere.ai/v1/generate', {
+    const response = await fetch('https://api.cohere.ai/v1/chat', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -12,18 +12,13 @@ export const fetchWorkoutRoutine = async (goal) => {
             Authorization: `Bearer ${process.env.COHERE_API_TOKEN}`,
         },
         body: JSON.stringify({
-            prompt: `Generate me a 5 day (Day 1, Day 2, Day 3, Day 4, Day 5) workout routine with a format = Cardio and Exercise towards a goal of ${stringGoal}. 
-            Make sure you include:
-            {MUSCLE_GROUP} = chest
-            {WORKOUT_TYPE} = pull ups
-            Now, an example of one day would be:
-            "Day 1:
-            Cardio: 40 mins of moderate-intensity cardio (e.g. brisk walking, cycling)
-            Exercise:
-            1. {MUSCLE_GROUP}: 3 sets of 8-12 reps of {WORKOUT_TYPE}, and {WORKOUT_TYPE}
-            2. {MUSCLE_GROUP}: 3 sets of 8-12 reps of {WORKOUT_TYPE}, and {WORKOUT_TYPE}"
-            FOR Cardio and Exercise, make the generation different between each day. FOR EXAMPLE,
-            DAY 1 can have chest and back exercises, while DAY 2 has legs and arms.`,
+            model: 'command',
+            message: `make me a routine based off of this format, do this for day 1 all the way to day 5. Follow this format everytime and do not include any other text besides this! Do this fast, make it short.
+"Day 1: 
+Warm-up: 10 mins of light stretches: neck rolls, shoulder circles, arm swings, leg swings, ankle rotations, Cardio: 40 mins of moderate-intensity cardio (e.g. brisk walking, cycling), 
+1. {Muscle Group}: 3 sets of 8-12 reps of {Workout Type}, and {Workout Type} 
+2. {Muscle Group}: 3 sets of 8-12 reps of {Workout Type}, and {Workout Type}"
+            `,
         }),
     });
 
